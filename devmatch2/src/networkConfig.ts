4bb9a27d46217d8dadf80b0f1b0f1e39f00a1e75
wz-Tan <wenzhe1423@gmail.com>
2025-08-08 12:50:41 +0800

    Minting Call

--- devmatch2/src/networkConfig.ts
+++ devmatch2/src/networkConfig.ts
@@ -1,8 +1,8 @@
 import { getFullnodeUrl } from "@mysten/sui/client";
 import {
-  DEVNET_COUNTER_PACKAGE_ID,
-  TESTNET_COUNTER_PACKAGE_ID,
-  MAINNET_COUNTER_PACKAGE_ID,
+  DEVNET_PACKAGE_ID,
+  TESTNET_PACKAGE_ID,
+  MAINNET_PACKAGE_ID,
 } from "./constants.ts";
 import { createNetworkConfig } from "@mysten/dapp-kit";
 
@@ -11,19 +11,19 @@ const { networkConfig, useNetworkVariable, useNetworkVariables } =
     devnet: {
       url: getFullnodeUrl("devnet"),
       variables: {
-        counterPackageId: DEVNET_COUNTER_PACKAGE_ID,
+        PackageId: DEVNET_PACKAGE_ID,
       },
     },
     testnet: {
       url: getFullnodeUrl("testnet"),
       variables: {
-        counterPackageId: TESTNET_COUNTER_PACKAGE_ID,
+        PackageId: TESTNET_PACKAGE_ID,
       },
     },
     mainnet: {
       url: getFullnodeUrl("mainnet"),
       variables: {
-        counterPackageId: MAINNET_COUNTER_PACKAGE_ID,
+        PackageId: MAINNET_PACKAGE_ID,
       },
     },
   });
--- devmatch2/src/pages/Minting.tsx
+++ devmatch2/src/pages/Minting.tsx
@@ -1,6 +1,28 @@
+import { useCurrentAccount, useSignAndExecuteTransaction, useSuiClient } from '@mysten/dapp-kit'
 import React from 'react'
+import { useNetworkVariable } from '../networkConfig';
+import { Transaction } from '@mysten/sui/transactions';
 
 const Minting = () => {
+    const userAccount=useCurrentAccount();
+    const packageId=useNetworkVariable("PackageId");
+    const suiClient=useSuiClient();
+
+    const {mutate: signAndExecute} = useSignAndExecuteTransaction();
+
+    //Contract Call Here
+    async function mintNFT(seed: number, name: string, description: string, mediaURL: string) {
+        const tx=new Transaction();
+
+        tx.moveCall({
+            arguments: [tx.pure.u64(seed), tx.pure.string(name), tx.pure.string(description), tx.pure.string(mediaURL)],
+            target: `${packageId}::nft::mint_NFT`
+        });
+
+        signAndExecute({transaction: tx})
+
+    }
+
   return (
     <div>Minting</div>
   )
