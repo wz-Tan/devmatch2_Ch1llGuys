b1f76d79098473dc6d6099e4533c2bb78236d0d0
ajian_nedo <asianerd28@gmail.com>
2025-08-09 17:30:13 +0800

    Limited height for images

--- devmatch2/src/components/OwnedNFTCard.tsx
+++ devmatch2/src/components/OwnedNFTCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const rarityBorderColors = {
   common: "border-black hover:border-gray-400",
@@ -19,21 +19,21 @@ const rarityTextColors = {
   mythic: "text-pink-500"
 };
 
-function OwnedNFTCard({ item }:{item:any}) {
-  let rarity:string=item.rarity.variant;
+function OwnedNFTCard({ item }: { item: any }) {
+  let rarity: string = item.rarity.variant;
   const borderColor = rarityBorderColors[rarity as keyof typeof rarityBorderColors] || "border-white";
   const textColor = rarityTextColors[rarity as keyof typeof rarityTextColors] || "text-white";
 
   return (
     <div className={`bg-[rgb(15,15,15)] rounded-xl hover:bg-[rgb(20,20,20)] transition-colors p-4 border ${borderColor} group h-full flex flex-col`}>
       {/* Image Container */}
-      <div className="aspect-square bg-gradient-to-br from-orange-400 to-red-600 flex items-center justify-center text-6xl rounded-lg mb-4">
-        <img src={item.mediaURL} className="w-full h-full object-cover"/>
+      <div className="w-full h-64 aspect-square bg-gradient-to-br from-orange-400 to-red-600 flex items-center justify-center text-6xl rounded-lg mb-4 overflow-hidden">
+        <img src={item.mediaURL} className="w-full h-full object-cover" />
       </div>
 
 
       <div className="flex-1 flex flex-col justify-between">
-        
+
         {/* Header Section  */}
         <div className="mb-3">
           <div className="flex justify-between items-start gap-2 mb-2">
@@ -57,14 +57,14 @@ function OwnedNFTCard({ item }:{item:any}) {
         </div>
 
         {/* Button Section  */}
-        <Link to="/owned-nft-details" state={{ collectionNFT: item}}>
+        <Link to="/owned-nft-details" state={{ collectionNFT: item }}>
           <div className="flex justify-center">
             <button className="relative z-10 text-orange-500 group-hover:bg-orange-500 group-hover:text-white group-hover:translate-y-8 transition duration-300 py-2 px-4 rounded-md font-medium transition-all duration-300 text-sm">
               Learn More
             </button>
           </div>
         </Link>
-        
+
       </div>
     </div>
   );
