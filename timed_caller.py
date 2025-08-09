728375aa60e2ba01713a8cf836f04c10d742b5b2
ajian_nedo <asianerd28@gmail.com>
2025-08-09 12:13:07 +0800

    Matched contract arguments

--- timed_caller.py
+++ timed_caller.py
@@ -5,11 +5,11 @@ import os
 
 load_dotenv()
 
-wallet_address = os.getenv('WALLET')
+house_address = os.getenv('HOUSE')
 package_id = os.getenv('PACKAGE')
 
 interval = int(input('Call frequency? (in minutes, default 60) : '))
 while True:
-    subprocess.call(['sui', 'client', 'call', '--function', 'globalTimedTasks', '--args', wallet_address, '--module', 'bidding', '--package', package_id])
+    subprocess.call(['sui', 'client', 'call', '--function', 'globalTimedTasks', '--args', house_address, '0x6', '--module', 'bidding', '--package', package_id])
     print(f'last executed at : {time.strftime('%m/%d/%Y, %H:%M:%S', time.localtime())} with {interval} minute interval')
     time.sleep(interval * 60)
